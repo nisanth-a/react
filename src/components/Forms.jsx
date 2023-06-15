29cd4e77985689910e438b408ab82ce404a274c1
Nisanth .A <nisanth.alla@acsicorp.com>
2023-06-15 11:45:19 +0530

    form file update

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -2,24 +2,44 @@ import React, { useState } from "react";
 
 function Forms() {
   const [name, setName] = useState("");
+  const [dropdown, setDropdown] = useState("React");
+
   function nameHandler(e) {
     setName(e.target.value);
+  }
+
+  function dropdownHandler(event) {
+    setDropdown(event.target.value);
+  }
+
+  function submitHandler() {
+    alert(`The submitted name is ${name}`);
   }
   return (
     <>
       <div className="ui header">Forms</div>
-      <form>
+      <form onSubmit={submitHandler}>
         <fieldset>
           <legend className="ui header">Forms in React</legend>
-          <label className="ui label">
+          <label className="ui label header">
             Name:
             <input
               type="text"
-              className="ui focus input"
+              className="ui focus input header"
               onChange={nameHandler}
             ></input>
           </label>
-          {name}
+          <br></br>
+          <br></br>
+          <select className="ui dropdown" onChange={dropdownHandler}>
+            <option value="React">React</option>
+            <option value="Angular">Angular</option>
+            <option value="Vue">Vue</option>
+          </select>
+          <br></br>
+          <div className="ui message">{dropdown}</div>
+          <br></br>
+          <button className="ui positive button">Submit</button>
         </fieldset>
       </form>
     </>
